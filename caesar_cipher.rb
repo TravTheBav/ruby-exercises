72e37e5284575acb61c85bbb255267e19533638f
Travis <tzito93@gmail.com>
2022-02-18 11:20:58 -0600

    caesar cipher style editing

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,17 +1,17 @@
 def caesar_cipher(str, shift_factor)
-    chars = str.split("")
-    chars.each_with_index do |char, idx|
-        if ("a".."z").include?(char)
-            char_code = ((char.ord - 97 + shift_factor) % 26) + 97
-        elsif ("A".."Z").include?(char)
-            char_code = ((char.ord - 65 + shift_factor) % 26) + 65
-        else
-            char_code = char.ord
-        end
-        chars[idx] = char_code.chr
+  chars = str.split('')
+  chars.each_with_index do |char, idx|
+    if ('a'..'z').include?(char)
+      char_code = ((char.ord - 97 + shift_factor) % 26) + 97
+    elsif ('A'..'Z').include?(char)
+      char_code = ((char.ord - 65 + shift_factor) % 26) + 65
+    else
+      char_code = char.ord
     end
-    chars.join
+    chars[idx] = char_code.chr
+  end
+  chars.join
 end
 
-puts caesar_cipher("What a string!", 5)
-puts caesar_cipher("ABxyz", 1)
+puts caesar_cipher('What a string!', 5)
+puts caesar_cipher('ABxyz', 1)
